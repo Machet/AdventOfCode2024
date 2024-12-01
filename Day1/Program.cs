e8c9124cb1c13e4c54c141b0f157ca4ed48cb1de
Mariusz Macheta <Mariusz.Macheta@gmail.com>
2024-12-01 09:06:35 +0100

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+var values = File.ReadAllLines("input.txt").Select(GetValues);
+var values1 = values.Select(x => x.Val1).Order();
+var values2 = values.Select(x => x.Val2).Order();
+
+var result1 = values1.Zip(values2, (v1, v2) => Math.Abs(v1 - v2)).Sum();
+Console.WriteLine(result1);
+
+var result2 = values1.Select(v1 => v1 * values2.Count(v2 => v2 == v1)).Sum();
+Console.WriteLine(result2);
+
+Input GetValues(string line)
+{
+	var values = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+	return new Input(int.Parse(values[0]), int.Parse(values[1]));
+}
+
+record Input(int Val1, int Val2);
